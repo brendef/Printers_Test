1bcfb897741f2d1bf61ed51028497a3ab6aa5be1
brendef <brendan.defaria@gmail.com>
2023-02-03 00:06:38 +0200

    first commit

---
+++ printers-api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+  api "printers-api/apiFunctions"
+)
+
+func main() {
+
+  router := gin.Default()
+
+  router.GET("/printers", api.GetPrinters)
+  router.GET("/printer/:ip", api.GetPrinterByIp)
+  router.POST("/add_printer", api.AddPrinter)
+  router.GET("/change_status/:ip", api.ChangePrinterStatus)
+  router.GET("/update_name/:ip", api.ChangePrinterName)
+
+  router.Run("localhost:8080")
+}
